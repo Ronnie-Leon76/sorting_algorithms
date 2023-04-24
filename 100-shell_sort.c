db50e287e94a4e160f93299c753a3e1e6abb24f3
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-24 08:11:20 -0700

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using shell sort
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < size /3; gap = gap * 3 + 1)
+		;
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
