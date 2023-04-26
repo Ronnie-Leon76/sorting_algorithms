63a6e6cdeb3c2075fd97c097dfdd7a48014efd74
Ronnie-Leon76 <ronleon76@gmail.com>
2023-04-25 23:45:36 -0700

    best case insertion sort O(n)

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - sorts a doubly linked list of integers
  * @list: doubly linked list of integers
  *
  * Returns: void
